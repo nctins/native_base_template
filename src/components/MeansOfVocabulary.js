a8f868c0681c867d1e90f3aa130b8f4c2a2ff8b4
vuprodtkt <vuprodtkt1111@gmail.com>
2022-05-20 21:48:51 +0700

    fix UI

--- src/components/MeansOfVocabulary.js
+++ src/components/MeansOfVocabulary.js
@@ -1,23 +1,34 @@
 import {View, TextInput } from 'react-native';
 import React from "react";
 import SelectComponent from './Select';
+import IconAwesome from '@expo/vector-icons/FontAwesome';
 
-const MeansOfVocabulary = () =>{
-
-    const styles = {
-        TextInputConfig:{
-            borderWidth:1,
-            marginLeft:10,
-            borderRadius:5,
-            paddingHorizontal:10,
-            paddingVertical:2,
-            height: 40
-        }
+const styles = {
+    TextInputConfig:{
+        borderWidth:1,
+        marginLeft:10,
+        borderRadius:5,
+        paddingHorizontal:10,
+        paddingVertical:2,
+        height: 40
     }
+}
+
+const MeansOfVocabulary = ({typeList,setTypeList,id}) =>{
+    const pressRemoveIcon = () =>{
+        setTypeList(typeList.filter((prev,index) => index !== id));
+    }
+
     return ( 
         <View style={{width:"100%",flexDirection:"row",marginTop:10,alignItems:'center'}}>
             <SelectComponent />
-            <TextInput numberOfLines={1} style={[styles.TextInputConfig,{width:"50%"}]}></TextInput>
+            <TextInput numberOfLines={1} style={[styles.TextInputConfig,{width:"50%"}]} placeholder="enter means"></TextInput>
+            <IconAwesome 
+                name='remove' 
+                size={30} 
+                style={{marginLeft:15}}
+                onPress={pressRemoveIcon}    
+            ></IconAwesome>
         </View>
      );
 }
