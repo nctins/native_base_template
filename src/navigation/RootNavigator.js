1644e411d03189208b19c40c78b5eca74f94c72d
nctins <nguyencongtin2000@gmail.com>
2022-05-28 17:13:22 +0700

    fix login flow

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,23 +1,78 @@
-import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useContext, useState, useCallback, useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppLoading from "expo-app-loading";
+import  * as SecureStore from "expo-secure-store";
 
 import StartScreen from "../screens/StartScreen";
 import LoginScreen from "../screens/LoginScreen";
 import SignUpScreen from "../screens/SignUpScreen";
 import DrawerNavigation from "./Drawer";
+import { AuthContext } from "../contexts/AuthContext";
 
 const Stack = createNativeStackNavigator();
 
-export const RootNavigator = function(){
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="LoginScreen">
-                <Stack.Screen name="StartScreen" component={StartScreen} options={{headerShown: false}}/>
-                <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}}/>
-                <Stack.Screen name="SignUpScreen" component={SignUpScreen} options={{headerShown: false}}/>
-                <Stack.Screen name="DrawerNavigation" component={DrawerNavigation} options={{headerShown: false}} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+export const RootNavigator = function () {
+  const authContext = useContext(AuthContext);
+  const [status, setStatus] = useState("loading");
+  const [initScreen, setInitScreen] = useState("StartScreen")
+  const loadJWT = useCallback(async () => {
+    try {
+      const token = await SecureStore.getItemAsync("token");
+      const { accessToken, refreshToken } = JSON.parse(token);
+
+      authContext.setAuthState({
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+        authenticated: true,
+      });
+      setStatus("success");
+    } catch (error) {
+      setStatus("error");
+      authContext.setAuthState({
+        accessToken: null,
+        refreshToken: null,
+        authenticated: false,
+      });
+    }
+  }, []);
+  useEffect(() => {
+    loadJWT();
+  }, []);
+
+  if (status === "loading") {
+    console.log("app loading")
+    return <AppLoading />;
+  }
+
+  if(authContext?.AuthState?.authenticated === true){
+      setInitScreen("DrawerNavigation")
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={initScreen}>
+        <Stack.Screen
+          name="StartScreen"
+          component={StartScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUpScreen"
+          component={SignUpScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="DrawerNavigation"
+          component={DrawerNavigation}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
